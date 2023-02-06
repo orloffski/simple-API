d63437a4e1f098462fd59f4d18ae42f18ba4d68c
Evgen Orlovski <orloffski@gmail.com>
2023-02-07 00:20:42 +0300

    feat: add userRouter

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const port = 8000;
-
 const app = express();
 
 app.get('/', (req, res) => {
 	res.send('Hello!');
 })
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
 	console.log(`Server started on http://localhost:${port}`);
